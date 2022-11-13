79847f4c36718f3ac5237d0cba11e3bd9b4c7815
Belén Roldán <belenroldaninfo@gmail.com>
2022-11-12 21:49:48 -0300

    update

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,14 +1,71 @@
+// Variables
 
-// Funcion que cierra sesion y redirecciona al login
-function logOut(){
-    localStorage.getItem("email");
-    localStorage.removeItem("email");
-    window.location = "login.html"
+
+
+// Funciones
+
+// Mostrar pagina solo si el usuario esta loggueado
+function showPage() {
+  if (!localStorage.getItem("email")) {
+    let timerInterval;
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      html: "Debe iniciar sesion para acceder a su perfil.",
+      timer: 3000,
+      didOpen: () => {
+        Swal.showLoading()
+        const b = Swal.getHtmlContainer().querySelector("b");
+        timerInterval = setInterval(() => {
+          b.textContent = Swal.getTimerLeft();
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      /* Read more about handling dismissals below */
+      if (result.dismiss === Swal.DismissReason.timer) {
+        window.location.href = "login.html";
+      }
+    });
+
+    
+    
+  }
+}
+// Muestra E-mail logueado
+function showEmail(){
+    email.innerHTML = localStorage.getItem("email");
 }
 
-// DOM
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById("logout").addEventListener("click",()=>{
-        logOut()
-    });
+
+//Validacion de Bootstrap
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
+
+
+document.addEventListener('DOMContentLoaded', function (){
+    showPage();
+    
+    let email = localStorage.getItem("email");
+    document.getElementById("email").innerHTML = email;
+    
 })
